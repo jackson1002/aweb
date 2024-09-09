019216e633fed887dce0a4b2d16a3b329e97d2f5
jackson1002 <102551009+jackson1002@users.noreply.github.com>
2024-09-09 11:00:04 -0300

    atualizei

---
+++ MEU_SITE_REACT/meu_site_react/src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+function Header(){
+    return(
+        <section>
+            <header>
+                <div className="header">
+                    <h1 className="iniciais">J.L</h1>
+                    <nav>
+                        <div className="ajeita_links">
+                            <a href="#Algumas curiosidades sobre mim" class="link">Curiosidades</a><br/>
+                            <a href="#Histórico" className="link">Histórico</a><br/>
+                            <a href="#Causos" className="link">Causos</a><br />
+                            <a href="#Habilidades" className="link">Habilidades</a><br/>
+                            <a href="#Contato" className="link">Contato</a><br/>
+                            <a href="#Minhas_fotos" className="link">Minhas fotos</a><br/><br/><br/><br/>
+                        </div>
+                    </nav>
+                </div>
+            </header>            
+        </section>
+    );
+}
+
+export default Header;
---
+++ MEU_SITE_REACT/meu_site_react/src/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import minhafoto1 from "./img/minha_foto1.jfif"
+
+function Hero(){
+    return(
+        <figure>
+            <div className="hero">
+                <img src = {minhafoto1} alt = 'Foto do autor do site,Jackson Lucas'/>
+                <h1 className="nome_completo">Jackson Lucas</h1>
+            </div>
+        </figure>
+    );
+}
+
+export default Hero;
